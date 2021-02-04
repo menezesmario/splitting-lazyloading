cde457f0023671cb0ff901208f8ab2d2863bcd21
Mário Menezes <mario082@gmail.com>
2021-02-04 07:34:25 -0300

    Rodape e Slogan

---
+++ src/Rodape.js
@@ -0,0 +1,11 @@
+function Rodape() {
+    return (
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">&copy;Recode 2021</a>    
+    );
+}
+
+export default Rodape;
---
+++ src/Slogan.js
@@ -0,0 +1,12 @@
+function Slogan() {
+    return (
+    <p>
+        <strong>
+            Recode Pro.
+        </strong>
+        Recodificando vidas através da Tecnologia.
+    </p>
+    );
+}
+
+export default Slogan;
